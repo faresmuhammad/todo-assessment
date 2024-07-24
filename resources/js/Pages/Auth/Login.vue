0ec12fca6e8e930dabfdc60dbd6c110ad921ba8d
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-07-25 00:11:14 +0300

    Modify Auth Forms Layout

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="container min-vh-100 justify-content-center">
+    <div class="container min-vh-100">
+        <div class="row min-vh-100 justify-content-center align-items-center">
 
-        <form method="post" @submit.prevent="login(data)" class="d-flex flex-column gap-4 my-auto">
+            <div class="col-6">
 
-            <div class="flex flex-column">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" v-model="data.email" required>
+                <form method="post" @submit.prevent="login(data)" class="d-flex flex-column gap-4 my-auto">
+
+                    <div class="flex flex-column">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email" v-model="data.email" required>
+                    </div>
+                    <div class="flex flex-column">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" v-model="data.password" required>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" name="remember" v-model="data.remember" class="form-check-input">
+                        <label for="remember" class="form-check-label">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary" style="display: block">Login</button>
+                </form>
+                <div class="my-3 text-center">
+                    <span>Not a member? </span>
+                    <router-link :to="{name: 'register'}">Register</router-link>
+                </div>
             </div>
-            <div class="flex flex-column">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" v-model="data.password" required>
-            </div>
-            <div class="form-check">
-                <input type="checkbox" name="remember" v-model="data.remember" class="form-check-input">
-                <label for="remember" class="form-check-label">Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-primary" style="display: block">Login</button>
-        </form>
-        <div class="my-3 text-center">
-            <span>Not a member? </span>
-            <router-link :to="{name: 'register'}">Register</router-link>
         </div>
     </div>
 
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="container min-vh-100 justify-content-center">
+    <div class="container min-vh-100">
+        <div class="row min-vh-100 justify-content-center align-items-center">
 
-        <form method="post" @submit.prevent="register(data)" class="d-flex flex-column gap-4 my-auto">
+            <div class="col-6">
+                <form method="post" @submit.prevent="register(data)" class="d-flex flex-column gap-4 my-auto">
 
-            <div class="flex flex-column">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" v-model="data.name" required>
-            </div>
-            <div class="flex flex-column">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" v-model="data.email" required>
-            </div>
-            <div class="flex flex-column">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" v-model="data.password" required>
-            </div>
-            <div class="flex flex-column">
-                <label for="password-confirmation" class="form-label">Password Confirmation</label>
-                <input type="password" name="password-confirmation" class="form-control"
-                       v-model="data.password_confirmation" required>
-            </div>
+                    <div class="flex flex-column">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" name="name" v-model="data.name" required>
+                    </div>
+                    <div class="flex flex-column">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" v-model="data.email" required>
+                    </div>
+                    <div class="flex flex-column">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" v-model="data.password" required>
+                    </div>
+                    <div class="flex flex-column">
+                        <label for="password-confirmation" class="form-label">Password Confirmation</label>
+                        <input type="password" name="password-confirmation" class="form-control"
+                               v-model="data.password_confirmation" required>
+                    </div>
 
-            <button type="submit" class="btn btn-primary">Register</button>
-        </form>
-        <div class="my-3 text-center">
-            <span>Already registered? </span>
-            <router-link :to="{name: 'login'}">Login</router-link>
+                    <button type="submit" class="btn btn-primary">Register</button>
+                </form>
+                <div class="my-3 text-center">
+                    <span>Already registered? </span>
+                    <router-link :to="{name: 'login'}">Login</router-link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
